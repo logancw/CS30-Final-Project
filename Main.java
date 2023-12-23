3cfd38006568ec90f90a39425e8f80ce5f1bb0a9
logancw <logan.weisgerber@rcsd.ca>
2023-12-22 23:44:49 -0600

    + Welcome Screen 12/22/2023

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Your code here
+        welcome();
     }
+
+    public static void welcome(){
+        Scanner kb = new Scanner(System.in);
+
+
+        System.out.println("Welcome to PIG. Would you like to read the rules, or get started? [1] Get Started [2] Rules");
+        int selection = kb.nextInt();
+        if(selection == 1 || selection == 2){
+            if(selection == 1){
+                getStarted();
+            }else{
+                rules();
+            }
+        }else{
+            welcome();
+        }
+    }
+
+    public static void rules(){}
+
+    public static void getStarted(){}
 }
