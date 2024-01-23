395cfe926ad34810ff57b76962d229c55d0024fe
Logan Weisgerber <logan.weisgerber@rcsd.ca>
2024-01-23 14:54:08 -0600

    + Added Comments + Added PlayAgain

--- DiceRoller.java
+++ DiceRoller.java
@@ -3,43 +3,44 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DiceRoller {
-    int round = 1;
-    int total = 0;
-    List<Integer> scoresP = new ArrayList<Integer>();
+    int round = 1; // Int var to see what round is being played
+    int total = 0; // The total the user / computer has collected in one round.
+    List<Integer> scoresP = new ArrayList<Integer>(); // These two ArrayLists are used to store the scores for the player and computer
     List<Integer> scoresC = new ArrayList<Integer>();
 
     public void rollDice() {
 
-        double ddice = Math.random() * 7;
-        int dice = (int) ddice;
+        double ddice = Math.random() * 7; // Random creates a double (4.5) which is a decimal number 
+        int dice = (int) ddice; // So we cast it as an int so we can get a whole number
 
         if (dice == 0) {
             dice = 1;
         }
 
-        System.out.println("You rolled a " + dice);
+        System.out.println("You rolled a " + dice); // Displays what the user rolled 
 
         if (dice == 1) {
-            System.out.println("You lose all your points for this round.");
-            compDice();
+            System.out.println("You lose all your points for this round."); // If the user rolls a 1 they lose all their points for that round
+            compDice(); // Moves on to the computers turn
 
         } else {
             total += dice;
-            System.out.println("Your total is " + total);
-            System.out.println("Would you like to roll again? [1] Yes [2] No");
+            System.out.println("Your total is " + total); // Displays the total the user has collected so far
+            System.out.println("Would you like to roll again? [1] Yes [2] No"); // Asks user if they would like to keep gambling their points or keep what they have. 
             Scanner kb = new Scanner(System.in);
             int selection = kb.nextInt();
             if (selection == 1) {
                 rollDice();
             } else {
-                scoresP.add(total);
+                scoresP.add(total); // Adds the total to the ArrayList
                 total = 0;
-                compDice();
+                compDice(); // Runs the computer
             }
         }
 
     }
 
+    // This function is the same as the player but adds more Random variables to allow the computer to choose how they want to play. 
     public void compDice() {
         // DICE
         double ddice = Math.random() * 7;
@@ -68,12 +69,13 @@ public class DiceRoller {
         
     }
 
+    // Results displays how many points computer and the player has after each round 
     public void results() {
 
-        // scoresP.add(100);
 
         int csum = 0;
         int psum = 0;
+        // A for loop is used to add up all the scores in the ArrayLists into one variable called psum or csum meaning computer sum or player sum
         for (int i = 0; i < scoresP.size(); i++) {
             psum += scoresP.get(i);
         }
@@ -84,14 +86,20 @@ public class DiceRoller {
 
         try {
             System.out.println("Player score is " + psum);
-            Thread.sleep(2000);
+            Thread.sleep(2000); // Thread.Sleep() allows a wait time inbetween each message being sent about scores. 
             System.out.println("Computer score is " + csum);
             Thread.sleep(2000);
-            if (psum >= 100 || csum >= 100) {
-                if(psum >= 100){
+            if (psum >= 100 || csum >= 100) { // Checks what player is over 100 points
+                if(psum >= 100){ // if player wins
                     System.out.println("Congratulations! You have won the game!");
-                }else{
+                    playAgain();
+                }else{ // computer wins
                     System.out.println("Computer has won the game!");
+                    playAgain();
+                } // or if its a tie. 
+                if(psum >= 100 && csum >= 100){
+                    System.out.println("It's a tie!");
+                    playAgain();
                 }
             } else {
                 round++;
@@ -100,18 +108,18 @@ public class DiceRoller {
                 rollDice();
             }
         } catch (InterruptedException e) {
-            System.out.println("Error");
+            System.out.println("Error"); // Random error message in case the TRY fails. 
         }
 
     }
 
-
+    // Displays a play again screen after the user has won or lost. 
     public void playAgain(){
         System.out.println("Would you like to play again? [1] Yes [2] No");
         Scanner kb = new Scanner(System.in);
         int selection = kb.nextInt();
         if(selection == 1){
-            round = 1;
+            round = 1; // Resets variables. 
             total = 0;
             scoresP.clear();
             scoresC.clear();
--- Main.java
+++ Main.java
@@ -14,22 +14,23 @@ public class Main {
     }
 
     public static void welcome(){
-        Scanner kb = new Scanner(System.in);
+        Scanner kb = new Scanner(System.in); // Grabs the scanner file
 
-
-        System.out.println("Welcome to PIG. Would you like to read the rules, or get started? [1] Get Started [2] Rules");
-        int selection = kb.nextInt();
-        if(selection == 1 || selection == 2){
-            if(selection == 1){
+        // STRING  MODULE 01
+        System.out.println("Welcome to PIG. Would you like to read the rules, or get started? [1] Get Started [2] Rules"); // Prompts the user if they would like to read the rules or just start playing 
+        int selection = kb.nextInt(); // VARIABLE MODULE 01 <- Creates a variable so the user can select an option in the string above
+        if(selection == 1 || selection == 2){ // CONDITIONAL MODULE 10
+            if(selection == 1){ // if its 1 getStarted() function runs
                 getStarted();
             }else if(selection == 2){
-                rules();
+                rules(); // if its 2 it displays the rules. 
             }
         }else{
             welcome();
         }
     }
 
+    // This function does the same as teh welcome except it displays the rules
     public static void rules(){
         System.out.println("Rules \n1. You roll only one dice. \n2. If you roll a one you lose all your points for that round. \n3. First to 100 points wins. Player vs Computer");
         System.out.println("Back [1] Exit [2]");
@@ -43,7 +44,7 @@ public class Main {
             rules();
         }
     }
-
+    // This function grabs the DiceRoller() Class and starts the game.
     public static void getStarted(){
         DiceRoller dr = new DiceRoller();
         dr.rollDice();
